6a7e3c0f2c7247fd92f2a90d08648460521ea884
Meidesu <antoniomeireles150@gmail.com>
2024-06-06 22:13:16 -0300

    feat: Adicionado as resource de item e drop de item pelo bau

--- Scripts/ItensManager.gd
+++ Scripts/ItensManager.gd
@@ -1,4 +1,6 @@
 extends Node
+
+## Classe responsável por gerenciar todo tipo de entidade de item
 class_name ItensManager
 
 ## Posição padrão de drop de itens.
@@ -12,17 +14,50 @@ var randInd: int;
 
 const chestPackage = preload("res://Scenes/chest.tscn")
 
+##TODO: depois trocar por um json ou csv
+var itensData: Array[Dictionary] = [
+	{ 
+		"itemName": "Maçã",
+		"iconPath": "res://icon.svg",
+		"modelPath": "res://Ultimate Stylized Nature - May 2022/glTF/Bush.gltf",
+	},
+	{ 
+		"itemName": "Pera",
+		"iconPath": "res://icon.svg",
+		"modelPath": "res://Ultimate Stylized Nature - May 2022/glTF/Bush_Small.gltf",
+	}
+];
+
+## Lista de todos os itens possíveis, em resource
+var itens: Array[ItemRes] = [];
+
 func _ready():
 	randomize();
 	randomizeSpawnSettings();
+	
+	loadItens()
 
 func _process(delta):
 	pass
 
-## Instancia um inimigo e adiciona como filho 
+## Transforma o csv ou json em um array de resource item
+func loadItens():
+	for item in itensData:
+		var _i = ItemRes.new();
+		
+		_i.itemName = item.itemName;
+		_i.iconPath = item.iconPath;
+		_i.modelPath = item.modelPath;
+		
+		itens.append(_i);
+
+## Instancia um baú e adiciona como filho 
 func dropChest(dropPosition: Vector2 = defaultDropPos):
 	
 	var _chest = chestPackage.instantiate();
+	
+	## O baú está atribuindo aléatoriamente um tipo de item a ele
+	_chest.setItemRes(pickRandomItem());
 	add_child(_chest);
 	
 	## Coloquei por enquanto, apenas pra obter um y satisfatório
@@ -34,8 +69,12 @@ func dropChest(dropPosition: Vector2 = defaultDropPos):
 	var _part = spawnParticlesScene.instantiate();
 	_part.global_position = _dropPosition;
 	Global.levelNode.add_child(_part);
-	
+	 
 	randomizeSpawnSettings()
+	
+func pickRandomItem():
+	var _item: ItemRes = itens.pick_random();
+	return _item;
 	
 ## Randomiza índice do monstro e posição de spawn:
 func randomizeSpawnSettings() -> void:
