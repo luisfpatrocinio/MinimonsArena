c2e109e611e562bc2444c2caa03a06ea6e799402
Meidesu <antoniomeireles150@gmail.com>
2024-06-11 22:33:41 -0300

    feat - Adicionado um modulo de camera

--- Scripts/dice.gd
+++ Scripts/dice.gd
@@ -1,24 +1,33 @@
 extends RigidBody3D
 
+## Um dado que retorna um valor entre 1-6
 class_name Dice
 
 signal rollFinished(side: int);
 
+## Força na qual o dado será arremessado
 @export var rollStrength: float = 15; 
 
 @onready var rayCasts = $RayCasts
 
+## Aplica rotação e uma força de impulso
 func roll():
-	transform.basis = Basis(Vector3.RIGHT, randf_range(0, 3 * PI)) * transform.basis;
-	transform.basis = Basis(Vector3.FORWARD, randf_range(0, 3 * PI)) * transform.basis;
-	transform.basis = Basis(Vector3.UP, randf_range(0, 3 * PI)) * transform.basis;
 	
-	var _impulseDir = Vector3(randf_range(-0.5, 0.5), 1, randf_range(-0.5, 0.5)).normalized();
-	angular_velocity = _impulseDir * rollStrength / 2  
+	#TODO: Melhorar essa rotação do dado
+	# aplica uma rotação e um impulso
+	transform.basis = Basis(Vector3.RIGHT, randf_range(0, 2 * PI)) * transform.basis;
+	transform.basis = Basis(Vector3.UP, randf_range(0, 2 * PI)) * transform.basis;
+	transform.basis = Basis(Vector3.FORWARD, randf_range(0, 2 * PI)) * transform.basis;
+	
+	var _impulseDir = Vector3(randf_range(-0.3, 0.3), 2, randf_range(-0.3, 0.3)).normalized();
+	angular_velocity = _impulseDir * rollStrength / 2	 
 	apply_central_impulse(_impulseDir * rollStrength);
 	
-	
+
+## Chamado quando o dado entra no modo [sleeping]
 func _on_sleeping_state_changed():
+	
+	# Pra saber se ele está parado
 	if sleeping:
 		var landedOnSide = false;
 		
@@ -27,6 +36,7 @@ func _on_sleeping_state_changed():
 				await get_tree().create_timer(.5).timeout;
 				rollFinished.emit(rayCast.oppositeSide);
 				landedOnSide = true;
-				
+					
+		# As vezes ele para pois trava no chão, nesse caso ele rola novamente
 		if !landedOnSide:
 			roll();
--- Scripts/dice_manager.gd
+++ Scripts/dice_manager.gd
@@ -1,25 +1,32 @@
 extends Node3D
 
-const dicePackage = preload("res://Scenes/dice.tscn")
+## Classe responsável por instanciar um [Dice] e tratar a resposta com base no banco de ações.
+class_name DiceManager
 
-# Called when the node enters the scene tree for the first time.
+# TODO: Remover depois
+@onready var camera: Camera = $Camera
+
+const dicePackage: PackedScene = preload("res://Scenes/dice.tscn")
+
+# TODO: remover quando não for mais preciso
 func _ready():
 	rollDice();
-	$Pivot/Camera3D.look_at($Pivot.global_position);
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+## Instancia um dado e retorna o valor ( Por enquanto ele tambem gerencia a camera, mas apenas para essa cena )
 func rollDice(posOrigin: Vector3 = Vector3.ZERO) -> int:
 	var _dice: Dice = dicePackage.instantiate();
-	add_child(_dice);
 	_dice.global_position = posOrigin;
+	add_child(_dice);
 	_dice.roll()
 	
 	# O sinal que retorna o valor
 	var response = await _dice.rollFinished;
 	
-	#$Pivot.create_tween().tween_property($Pivot, "global_position", _dice.global_position, 1);
+	## ta aqui por enquanto
+	camera.moveTo(_dice.global_position);
+	await camera.moveToFinished;
+	
+	var _zoom = 5;
+	camera.setSpringLength(_zoom);
 	
 	return response;
